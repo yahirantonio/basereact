2ee82ff75220b1aba118a0655fa6b7d0efca803d
yahirantonio <yahirantoni90@gmail.com>
2021-07-28 17:23:18 -0500

    inicio del proyecto

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../assets/styles/App.scss';
+
+// class helloworld extends Component {
+//   render() {
+//     return (
+//       <View />
+//     );
+//   }
+// }
+
+const HelloWorld = () =>(
+	<h1>Hola Mundo</h1>
+)
+
+export default HelloWorld;
